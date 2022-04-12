c1302b7239ed5f0b0988d4a2ac3ff1ea930a9820
Piotr <piotr.ostrowski@aol.com>
2022-04-12 21:56:55 +0200

    clear up redundant stuff

--- test/ownership.test.ts
+++ test/ownership.test.ts
@@ -1,13 +1,12 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { Okemonos } from "../typechain/Okemonos";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
-describe("Okemonos - ownership", () => {
+describe("ownership", () => {
   let signers: SignerWithAddress[];
   let owner: string;
   let ownerAcc: SignerWithAddress;
-  let contract: Okemonos;
+  let contract: any;
 
   before(async () => {
     signers = await ethers.getSigners();
@@ -16,20 +15,16 @@ describe("Okemonos - ownership", () => {
   });
 
   beforeEach(async () => {
-    const Okemonos = await ethers.getContractFactory("Okemonos");
-    contract = (await Okemonos.deploy()) as Okemonos;
+    const Artifact = await ethers.getContractFactory("Okemonos");
+    contract = (await Artifact.deploy()) as any;
     await contract.deployed();
   });
-  beforeEach(async () => {
-    const Okemonos = await ethers.getContractFactory("Okemonos");
-    contract = (await Okemonos.deploy()) as Okemonos;
-    await contract.deployed();
-  });
+
   it("only owner can transfer ownership", async () => {
     const newOwner = signers[1].address;
-    await expect(
-      contract.transferOwnership(newOwner, { from: signers[2].address }),
-    ).to.be.reverted;
+    const from = signers[2].address;
+    const tx = contract.transferOwnership(newOwner, { from });
+    await expect(tx).to.be.reverted;
   });
 
   it("should be able to transfer ownership", async () => {
@@ -37,8 +32,8 @@ describe("Okemonos - ownership", () => {
     contractOwner = await contract.owner();
     expect(contractOwner).to.equal(owner);
     const newOwner = signers[1].address;
-    const changeOwnershipTx = await contract.transferOwnership(newOwner);
-    await changeOwnershipTx.wait();
+    const tx = await contract.transferOwnership(newOwner);
+    await tx.wait();
     contractOwner = await contract.owner();
     expect(contractOwner).to.equal(newOwner);
   });
